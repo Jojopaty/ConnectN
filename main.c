bdd5331c414129997b5d8120e9093995e7718056
Edgar Boule <edgar.boule@gmail.com>
2021-05-14 00:05:13 +0200

    Added EXIT_SUCCESS when "quit" is choosen.

--- main.c
+++ main.c
@@ -17,15 +17,17 @@ int main(int argc, char *argv[])
     switch (startGame())
     {
     case 1:
-    printf(BLU "Starting the game\n" RESET);
+        printf(BLU "Starting the game\n" RESET);
         break;
     case 2:
-    printf(BLU "Loading last saved game\n" RESET);
+        printf(BLU "Loading last saved game\n" RESET);
         break;
     case 3:
-    printf(BLU "Bye\n" RESET);
+        printf(BLU "Bye\n" RESET);
+        return EXIT_SUCCESS;
         break;
     default:
+    // Should never be executed
         break;
     }
 
